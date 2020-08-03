d9f0d1dc6a74d58c9cc5cdd605afb887c62b7e50
nikolaygelemerov <nikolaygelemerov@gmail.com>
2020-08-03 20:12:37 +0300

    [TB-DS-7] create binary-search-tree folder

---
+++ src/binary-search-tree/index.test.ts
@@ -0,0 +1,54 @@
+import { BinarySearchTree, Data } from "./index";
+
+let root: BinarySearchTree<Data>;
+let rootData: Data;
+
+describe("BinarySearchTree", () => {
+  beforeEach(() => {
+    rootData = { item: "Root", value: 2 };
+    root = new BinarySearchTree(rootData);
+
+    root.insert({ item: null, value: 4 });
+    root.insert({ item: null, value: 1 });
+    root.insert({ item: null, value: 5 });
+    root.insert({ item: null, value: -5 });
+    root.insert({ item: null, value: 10 });
+    root.insert({ item: null, value: 20 });
+  });
+
+  test("insert", () => {
+    expect(root.left.data.value).toBe(1);
+  });
+
+  test("contains", () => {
+    expect(root.contains(root)).toBe(true);
+    expect(root.contains(new BinarySearchTree({ item: null, value: 5 }))).toBe(
+      true
+    );
+    expect(root.contains(new BinarySearchTree({ item: null, value: 1 }))).toBe(
+      true
+    );
+    expect(root.contains(new BinarySearchTree({ item: null, value: 12 }))).toBe(
+      false
+    );
+  });
+
+  test("validate", () => {
+    expect(root.validate()).toBe(true);
+
+    const fakeBinarySearchTree_1 = {
+      data: <Data>{ item: "Root", value: 2 },
+      left: { data: <Data>{ item: null, value: 22 }, validate: root.validate },
+      validate: root.validate,
+    };
+
+    const fakeBinarySearchTree_2 = {
+      data: <Data>{ item: "Root", value: 2 },
+      right: { data: <Data>{ item: null, value: 1 }, validate: root.validate },
+      validate: root.validate,
+    };
+
+    expect(fakeBinarySearchTree_1.validate()).toBe(false);
+    expect(fakeBinarySearchTree_2.validate()).toBe(false);
+  });
+});
---
+++ src/binary-search-tree/index.ts
@@ -0,0 +1,84 @@
+export type Data = {
+  item: any;
+  value: number;
+};
+
+export class BinarySearchTree<T> {
+  public data: Data;
+  public left: BinarySearchTree<Data> | null;
+  public right: BinarySearchTree<Data> | null;
+
+  constructor(data: Data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  /**
+   * Inserts a new node in the tree.
+   *
+   * Time complexity: O(log n)
+   * Space complexity: O(1)
+   *
+   * @returns {void}
+   */
+  public insert(data: Data): void {
+    if (this.data.value > data.value && this.left) {
+      this.left.insert(data);
+    } else if (this.data.value > data.value) {
+      this.left = new BinarySearchTree(data);
+    } else if (this.data.value < data.value && this.right) {
+      this.right.insert(data);
+    } else if (this.data.value < data.value) {
+      this.right = new BinarySearchTree(data);
+    }
+  }
+
+  /**
+   * Returns if a node already exists in the tree.
+   *
+   * Time complexity: O(log n)
+   * Space complexity: O(1)
+   *
+   * @returns {boolean}
+   */
+  public contains(node: BinarySearchTree<Data>): boolean {
+    if (this.data.value === node.data.value) {
+      return true;
+    } else if (this.data.value < node.data.value && this.right) {
+      return this.right.contains(node);
+    } else if (this.data.value > node.data.value && this.left) {
+      return this.left.contains(node);
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Returns if is valid binary search tree.
+   *
+   * Time complexity: O(log n)
+   * Space complexity: O(1)
+   *
+   * @returns {boolean}
+   */
+  public validate(min: number | null = null, max: number | null = null) {
+    if (min && this.data.value > min) {
+      return false;
+    }
+
+    if (max && this.data.value < max) {
+      return false;
+    }
+
+    if (this.left && !this.left.validate(this.data.value, max)) {
+      return false;
+    }
+
+    if (this.right && !this.right.validate(min, this.data.value)) {
+      return false;
+    }
+
+    return true;
+  }
+}
